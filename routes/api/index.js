f99d76df1660139ef681b46e5920d18a0d817b2a
penaone <penaone@gmail.com>
2020-10-28 21:54:35 -0700

    comments model and routes in progress

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
-
+const commentRoutes =  require('./comment-routes');
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 module.exports = router;
