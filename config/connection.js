d3c7a83deced43c54a8dd0523dfa6f4e61583c7c
penaone <penaone@gmail.com>
2020-10-30 10:49:51 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -13,11 +13,6 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-// create connection to our db
-//const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   port: 3306
-// });
+
 
 module.exports = sequelize;
